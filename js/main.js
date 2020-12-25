691848f5d490c92c00df0b05a92b67aa55a6fc27
JorgeQ <alejandrojj2018@gmail.com>
2020-12-24 19:45:38 -0500

    finalizado Ya

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import countDown from './countDown.js';
 import openLt from './open_letter.js';
 const d = document; 
 d.addEventListener('DOMContentLoaded',(e) =>{
-countDown('ctn-time-back','Dec 24, 2020 22:00:00','preview.js','preview.css')
+countDown('ctn-time-back','Dec 24, 2020 22:05:00','preview.js','preview.css')
 openLt('lt.css')
 closeLt('preview.css')
 })
